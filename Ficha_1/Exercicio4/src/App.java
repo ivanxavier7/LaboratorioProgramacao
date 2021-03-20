c1bfa3b4aa83b5358446184bc91da656bc1c8e4f
Ivan Xavier <ivanxavier123@gmail.com>
2021-03-20 09:31:04 +0000

    Exercicio4 Template

---
+++ Ficha_1/Exercicio4/src/App.java
@@ -0,0 +1,15 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        String v =  sc.nextLine();
+        String[] arrayV = v.split(" ");
+        for (String x : arrayV) {
+            int y = Integer.parseInt(x);
+            System.out.println("RESULTADO: " + y);
+        }
+    }
+}
---
+++ Ficha_1/Exercicio4/src/BlackNight.java
@@ -0,0 +1,22 @@
+public class BlackNight {
+    private int nrPromocoes;
+    private int qtPacotes;
+    private int pacotes;
+    private double saldo;
+
+    private void setNrPromocoes(int nrPromocoes) {
+        this.nrPromocoes = nrPromocoes;
+    }
+
+    private void setQtPacotes(int qtPacotes) {
+        this.qtPacotes = qtPacotes;
+    }
+
+    private void setPacotes(int pacotes) {
+        this.pacotes = pacotes;
+    }
+
+    private void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+}
