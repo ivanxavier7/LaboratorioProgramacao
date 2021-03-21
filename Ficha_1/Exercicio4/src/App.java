af2d177f9e92f460c82684aa787c9a68de4feab5
simao2silva <simao.silva@ua.pt>
2021-03-21 16:05:50 +0000

    teste

--- Ficha_1/Exercicio4/src/App.java
+++ Ficha_1/Exercicio4/src/App.java
@@ -5,7 +5,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        String v =  sc.nextLine();
+        String v = sc.nextLine();
         String[] arrayV = v.split(" ");
         for (String x : arrayV) {
             int y = Integer.parseInt(x);
@@ -13,3 +13,4 @@ public class App {
         }
     }
 }
+// teste
