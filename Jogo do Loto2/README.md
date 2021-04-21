2810d6ef07aa02d9cc398c0a186709649c8d7f3a
Ivan Xavier <ivanxavier123@gmail.com>
2021-04-21 14:21:41 +0100

    Diagrama de Classes
    Diagrama de Classes

---
+++ Jogo do Loto2/README.md
@@ -0,0 +1,82 @@
+# Diagrama de classes
+![Alt text](imt/JogoLotoClasses.png?raw=true "Diagrama de Classes")
+
+# Codificar o código
+```
+javac -encoding UTF8 src/*
+```
+# Correr o código
+```
+java -cp . App
+```
+
+# LaboratorioProgramacao
+Exercícios e trabalhos realizados no âmbito da unidade curricular "Laboratório de Programação"
+
+# Jogo do Loto
+
+### Cartão
+* Composto por uma matriz 3x9 (3D)
+* Cada linha tem sempre 5 números
+* Cada coluna tem entre 0 e 3 números
+* 1ª coluna de 1 a 9
+* 2ª até á 8ª coluna em intervalos de x0 a x9
+* 8ª coluna de 80 a 90
+
+### Jogo
+* Apenas um cartão
+* Representação na consola
+* Sorteio independente do jogo
+* Assinalar o cartão com o número obtido
+* Avisar sempre que a linha fica completa
+* No final apresentar mensagem de conclusão
+* Após a conclusão pode reiniciar ou continuar com o mesmo cartão
+
+### Documentação
+* Documentação JavaDoc do programa
+* Indicar como executar a aplicação sem recurso ao IDE
+
+## Implementação
+
+### Jogo
+
+Variáveis
+
+    Array 2D
+    ArrayCópia 2D
+    Número escolhido
+
+Métodos
+
+    Gerar o cartão
+    Trocar Repetidos
+    Colocar Espaços em Branco
+    Pedir Cartão
+    Pedir Menu
+    Atribuir número escolhido
+    Copiar o cartão original
+    Verificar conclusão do cartão
+    Reiniciar o Jogo
+    
+Validação simples e de exceções
+
+Percorrer listas bidimensionais 
+
+```java
+    public void getClone(int[][] cartaoCopia, int[][] cartao) {
+        for (int lin = 0; lin<3; lin++) {
+            for (int col = 0; col < 9; col++) {
+                cartaoCopia[lin][col] = cartao[lin][col];
+            }
+        }
+    }
+```
+
+### Jogador
+* Menu
+* Implementa objeto do jogo
+* Recebe cartão
+* Introduz números
+* Reinicia ou Saí
+
+
---
+++ Jogo do Loto2/src/App.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+/**
+ * Lotto Game developed for the curricular unit in Programming Laboratories
+ * <p>
+ * Main Application Class
+ * @author Ivan Xavier - 92441
+ * @author Simão Silva - 102914
+ * @version 1.0
+ */
+public class App {
+    /**
+     * cartao - array used to store the card received by the game
+     * cartaoOriginal - array used to hold the copy of the card received by the game
+     */
+    private static int[][] cartao = new int[3][9];
+    private static int[][] cartaoOriginal = new int[3][9]; 
+       
+    /**
+     * Main application
+     * <p>
+     * In this class we have the interactions between the player and the game,
+     * as well as some handling of errors and exceptions.
+     * <p>
+     * The card order is made automatically, the player only has to enter numbers and decide
+     * whether to play with a new card, keep the same card or leave.
+     * @param args
+     * @throws Exception simple error message, printed on the console 
+     * @since 1.0
+     */
+    public static void main(String[] args) throws Exception {  
+
+         
+        
+        try {
+            Game jogo = new Game();
+            Boolean run = true;  
+            System.out.println(jogo.getStringMenu());
+            cartao = jogo.generateCartao();
+            jogo.getClone(cartaoOriginal, cartao);
+            while (run) {
+                try {    
+                    Scanner scan = new Scanner(System.in);
+                    // Print the card on the console
+                    System.out.println("\nCartão recebido:" + jogo.getStringCartao(cartao)); 
+                    // Prompts the user to enter a number
+                    System.out.println("\nIntroduza o número sorteado:");
+                    int numero = scan.nextInt();   
+                    jogo.setNrEscolhido(numero, cartao);
+                    // Asks if the card is complete
+                    if(jogo.isConclusaoCartao(cartao)) {
+                        try {
+                        System.out.println("--------- GANHOU O JOGO! ---------\n");
+                        // Print the original card on the console
+                        System.out.println("Cartão Original:\n" + jogo.getStringCartao(cartaoOriginal));
+                        // Restart the game depending on the answer
+                        while (true) {
+                            // Asks the user if he wants to continue playing
+                            System.out.println("Deseja continuar a jogar com o mesmo cartão? S/n ou Sair");
+                            String resposta = scan.next();
+                            if(jogo.restartGame(resposta).equals("sim")){
+                                jogo.getClone(cartao, cartaoOriginal);
+                                break;
+                            } else if(jogo.restartGame(resposta).equals("sair")) {
+                                System.out.println("Obrigado, volte sempre!");
+                                run = false;
+                                break;
+                            } else if(jogo.restartGame(resposta).equals("nao")){
+                                cartao = jogo.generateCartao();
+                                jogo.getClone(cartaoOriginal, cartao);
+                                break;   
+                            } else{
+                                System.out.println("Por favor escolha \"Sim\", \"Não\" ou \"Sair!\"");
+                                continue;
+                            }
+                        }
+                    } catch (Exception e) {
+                        System.out.println("Obrigado por jogar o Loto, volte sempre!");
+                    }                                                                     
+                    }    
+                } catch (Exception e) {
+                    System.out.println("Ocorreu um erro! Por favor digite um valor válido: ");
+                }                    
+            } 
+            } catch (Exception e) {
+                System.out.println("Existe um erro dentro do jogo!");
+        }   
+    }
+}
+
