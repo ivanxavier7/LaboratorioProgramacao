85b7382805b82709ab20302203c97d274167997e
ivanxavier7 <ivanxavier@ua.pt>
2021-05-26 15:31:42 +0100

    Verificar Repetidos
    Verificar Repetidos

--- JogoLoto_Jogador/src/gui/util/Grid.java
+++ JogoLoto_Jogador/src/gui/util/Grid.java
@@ -1,7 +1,6 @@
 package gui.util;
 
-import java.util.Arrays;
-
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
@@ -74,65 +73,11 @@ public class Grid {
 		    Integer colIndex = GridPane.getColumnIndex(node);
 		    Integer rowIndex = GridPane.getRowIndex(node);
     	    if (node instanceof TextField) {
+    	    	// Count White Spaces
     	    	if(!((TextField)node).getText().equals("")) {
 	    	    	int textFieldNr = Integer.parseInt(((TextField)node).getText());
-	    	    	System.out.println("error " +  textFieldNr);
-	    	        switch (colIndex) {
-	    	        case 1:
-	    	        	if(textFieldNr < 1 || textFieldNr > 9) {
-	    	        		Controller.changeMessage(logger, String.format("O valor: %d na %dª coluna e na %dª linha é inválido!", textFieldNr, colIndex, rowIndex));
-	    	        		valid = false;
-	    	        	}
-	    	          break;
-	    	        case 2:
-	    	        	if(textFieldNr < 10 || textFieldNr > 19) {
-	    	        		Controller.changeMessage(logger, String.format("O valor: %d na %dª coluna e na %dª linha é inválido!", textFieldNr, colIndex, rowIndex));
-	    	        		valid = false;
-	    	        	}
-	    	          break;
-	    	        case 3:
-	    	        	if(textFieldNr < 20 || textFieldNr > 29) {
-	    	        		Controller.changeMessage(logger, String.format("O valor: %d na %dª coluna e na %dª linha é inválido!", textFieldNr, colIndex, rowIndex));
-	    	        		valid = false;
-	    	        	}
-	    	          break;
-	    	        case 4:
-	    	        	if(textFieldNr < 30 || textFieldNr > 39) {
-	    	        		Controller.changeMessage(logger, String.format("O valor: %d na %dª coluna e na %dª linha é inválido!", textFieldNr, colIndex, rowIndex));
-	    	        		valid = false;
-	    	        	}
-	    	          break;
-	    	        case 5:
-	    	        	if(textFieldNr < 40 || textFieldNr > 49) {
-	    	        		Controller.changeMessage(logger, String.format("O valor: %d na %dª coluna e na %dª linha é inválido!", textFieldNr, colIndex, rowIndex));
-	    	        		valid = false;
-	    	        	}
-	    	          break;
-	    	        case 6:
-	    	        	if(textFieldNr < 50 || textFieldNr > 59) {
-	    	        		Controller.changeMessage(logger, String.format("O valor: %d na %dª coluna e na %dª linha é inválido!", textFieldNr, colIndex, rowIndex));
-	    	        		valid = false;
-	    	        	}
-	    	          break;
-	    	        case 7:
-	    	        	if(textFieldNr < 60 || textFieldNr > 69) {
-	    	        		Controller.changeMessage(logger, String.format("O valor: %d na %dª coluna e na %dª linha é inválido!", textFieldNr, colIndex, rowIndex));
-	    	        		valid = false;
-	    	        	}
-	    	          break;
-	    	        case 8:
-	    	        	if(textFieldNr < 70 || textFieldNr > 79) {
-	    	        		Controller.changeMessage(logger, String.format("O valor: %d na %dª coluna e na %dª linha é inválido!", textFieldNr, colIndex, rowIndex));
-	    	        		valid = false;
-	    	        	}
-	    	          break;
-	    	        case 9:
-	    	        	if(textFieldNr < 80 || textFieldNr > 90) {
-	    	        		Controller.changeMessage(logger, String.format("O valor: %d na %dª coluna e na %dª linha é inválido!", textFieldNr, colIndex, rowIndex));
-	    	        		valid = false;
-	    	        	}
-	    	          break;
-	    	        }
+	    	    	// Column Validation
+	    	    	valid = columnValidation(textFieldNr, colIndex, rowIndex, logger);
 	    	    } else {
 	    	    	whiteSpaces ++;
 	    	    }
@@ -145,18 +90,102 @@ public class Grid {
     		Controller.changeMessage(logger, String.format("Existe(m) %d espaço(s) em branco a mais", 12 - whiteSpaces));
     		valid = false;
     	}
+	    // Repeated Validation
+	    valid = repeatedValidation(grid, logger);
     	return valid;
     }
     
-    private void writeGrid(GridPane grid) {
-    	System.out.println("\nCartão recebido:" + jogador.getStringCartao(cartao));
+    public static Node getItemByIndex (final int row, final int col, GridPane grid) {
+        Node result = null;
+        ObservableList<Node> childrens = grid.getChildren();
+        for (Node node : childrens) {
+            if(GridPane.getRowIndex(node) == row && GridPane.getColumnIndex(node) == col) {
+                result = node;
+                break;
+            }
+        }
+        return result;
+    }
+    
+    public static Boolean repeatedValidation(GridPane grid, Label logger) {
+    	Boolean validNr = true;
     	for (Node node : grid.getChildren()) {
-    	    System.out.println("Id: " + node.getId());
+		    Integer colIndex = GridPane.getColumnIndex(node);
+	        String rowNr1 = ((TextField)getItemByIndex(1, colIndex, grid)).getText();
+	        String rowNr2 = ((TextField)getItemByIndex(2, colIndex, grid)).getText();
+	        String rowNr3 = ((TextField)getItemByIndex(3, colIndex, grid)).getText();
     	    if (node instanceof TextField) {
-    	        // clear
-    	    	((TextField)node).setText("TESTE");
+    	        if(!rowNr1.equals("") && rowNr1.equals(rowNr2) || !rowNr1.equals("") && rowNr1.equals(rowNr3)) {
+    	        	Controller.changeMessage(logger, String.format("O número %s está repetido na %d coluna", rowNr1, colIndex));
+    	        	validNr = false;
+    	        } else if(!rowNr2.equals("") && rowNr2.equals(rowNr3)) {
+    	        	Controller.changeMessage(logger, String.format("O número %s está repetido na %d coluna", rowNr2, colIndex));
+    	        	validNr = false;
+    	        }
     	    }
     	}
+    	return validNr;
+    }
+    
+    private static Boolean columnValidation(int textFieldNr, int colIndex, int rowIndex, Label logger) {
+    	Boolean validNr = true;
+        switch (colIndex) {
+        case 1:
+        	if(textFieldNr < 1 || textFieldNr > 9) {
+        		Controller.changeMessage(logger, String.format("O valor: %d na %dª coluna e na %dª linha é inválido!", textFieldNr, colIndex, rowIndex));
+        		validNr = false;
+        	}
+          break;
+        case 2:
+        	if(textFieldNr < 10 || textFieldNr > 19) {
+        		Controller.changeMessage(logger, String.format("O valor: %d na %dª coluna e na %dª linha é inválido!", textFieldNr, colIndex, rowIndex));
+        		validNr = false;
+        	}
+          break;
+        case 3:
+        	if(textFieldNr < 20 || textFieldNr > 29) {
+        		Controller.changeMessage(logger, String.format("O valor: %d na %dª coluna e na %dª linha é inválido!", textFieldNr, colIndex, rowIndex));
+        		validNr = false;
+        	}
+          break;
+        case 4:
+        	if(textFieldNr < 30 || textFieldNr > 39) {
+        		Controller.changeMessage(logger, String.format("O valor: %d na %dª coluna e na %dª linha é inválido!", textFieldNr, colIndex, rowIndex));
+        		validNr = false;
+        	}
+          break;
+        case 5:
+        	if(textFieldNr < 40 || textFieldNr > 49) {
+        		Controller.changeMessage(logger, String.format("O valor: %d na %dª coluna e na %dª linha é inválido!", textFieldNr, colIndex, rowIndex));
+        		validNr = false;
+        	}
+          break;
+        case 6:
+        	if(textFieldNr < 50 || textFieldNr > 59) {
+        		Controller.changeMessage(logger, String.format("O valor: %d na %dª coluna e na %dª linha é inválido!", textFieldNr, colIndex, rowIndex));
+        		validNr = false;
+        	}
+          break;
+        case 7:
+        	if(textFieldNr < 60 || textFieldNr > 69) {
+        		Controller.changeMessage(logger, String.format("O valor: %d na %dª coluna e na %dª linha é inválido!", textFieldNr, colIndex, rowIndex));
+        		validNr = false;
+        	}
+          break;
+        case 8:
+        	if(textFieldNr < 70 || textFieldNr > 79) {
+        		Controller.changeMessage(logger, String.format("O valor: %d na %dª coluna e na %dª linha é inválido!", textFieldNr, colIndex, rowIndex));
+        		validNr = false;
+        	}
+          break;
+        case 9:
+        	if(textFieldNr < 80 || textFieldNr > 90) {
+        		Controller.changeMessage(logger, String.format("O valor: %d na %dª coluna e na %dª linha é inválido!", textFieldNr, colIndex, rowIndex));
+        		validNr = false;
+        	}
+          break;
+        }
+        return validNr;
     }
     
     public static void lockGrid(GridPane grid, Label logger) {
